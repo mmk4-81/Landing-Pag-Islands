29f63201e1dc3d824de415560ccc9c7bba3bfa01
mmk <mmkkarbalaei@gmail.com>
2024-03-05 21:37:39 +0330

    show menu function

---
+++ main.js
@@ -0,0 +1,14 @@
+//show menu
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    //validate that variables exist
+    if(toggle & nav){
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show-menu');
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
