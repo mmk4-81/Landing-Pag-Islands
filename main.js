da530d81f52904c2c5b219786e26212c2d72065a
mmk <mmkkarbalaei@gmail.com>
2024-03-05 21:51:06 +0330

    fixed bug

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 //show menu
+
 const showMenu = (toggleId,navId) =>{
     const toggle = document.getElementById(toggleId);
     const nav = document.getElementById(navId);
-
+console.log('AA');
     //validate that variables exist
-    if(toggle & nav){
+    if(toggle && nav){
         toggle.addEventListener('click',()=>{
             nav.classList.toggle('show-menu');
         })
     }
 }
 
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle','nav-menu');
